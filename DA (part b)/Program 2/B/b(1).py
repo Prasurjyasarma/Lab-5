4e7b63b94d70eeb0752a547c261b42f9cd6baae0
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2024-01-26 14:24:42 +0530

    Update b(1).py

--- DA (part b)/Program 2/B/b(1).py
+++ DA (part b)/Program 2/B/b(1).py
@@ -1,5 +1,6 @@
 from scipy.stats import f_oneway
+# Load the dataset
 df = pd.read_csv('StudentsPerformance.csv')
 # Extract math scores for each ethnicity_groups
 ethnicity_groups = df['ethnicity'].unique()
-ethnicity_group
+ethnicity_groups
