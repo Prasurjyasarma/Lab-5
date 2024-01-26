3cbd178bea08c8492feccc2d06367604a4b7a42c
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2024-01-26 13:50:30 +0530

    Update a(1).py

--- DA (part b)/Program 2/A/a(1).py
+++ DA (part b)/Program 2/A/a(1).py
@@ -1,9 +1,7 @@
 from scipy.stats import ttest_ind
-# Load the dataset
-df = pd.read_csv('StudentsPerformance.csv')
-# Separate data for male and female students
 from scipy.stats import ttest_ind
 import pandas as pd
-# Separate data for male students
+
+df = pd.read_csv('StudentsPerformance.csv')
 male_scores = df[df['gender'] == 'male']['math score']
 male_scores
